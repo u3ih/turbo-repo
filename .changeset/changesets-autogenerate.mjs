201c48429eaf3c26ead4d278746501573dee6efd
hieuchan <hieutt@melisoft.vn>
2025-08-03 19:29:05 +0700

    fix(i18n): remove log

--- .changeset/changesets-autogenerate.mjs
+++ .changeset/changesets-autogenerate.mjs
@@ -66,7 +66,7 @@ if (changeType && description) {
     }
 
     const changesetContent = `---
-'@carfromjapan/builder-${packageName}': ${changeType}
+'@carfromjapan/${packageName}': ${changeType}
 ---
 ${description}
 `;
--- packages/i18n/CHANGELOG.md
+++ packages/i18n/CHANGELOG.md
@@ -1,5 +1,11 @@
 ## 0.1.0
 
+## 2.0.2
+
+### Patch Changes
+
+- add new log message
+
 ## 2.0.1
 
 ### Patch Changes
